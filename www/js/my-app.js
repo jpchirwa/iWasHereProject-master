e552a1a0d43d1b4707a9f92e1e067e1d041a41b6
John-Paul Chirwa <johnpaulchirwa@live.co.uk>
2018-04-06 21:07:43 +0100

    Map Working on device ready

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -32,6 +32,7 @@ function onLoad(){
     }
 }
 function onDeviceReady() {
+    map();
     window.plugin.lightsensor.getReading(
 function success(reading) {
     console.log(JSON.stringify(reading));
@@ -133,4 +134,78 @@ function info() {
 
     var watchID = navigator.geolocation.getCurrentPosition(onSuccess, onError,{enableHighAccuracy: true ,timeout: 30000});
 
+}
+var Latitude = undefined;
+var Longitude = undefined;
+// Get geo coordinates
+function map() {
+    navigator.geolocation.getCurrentPosition
+    (onMapSuccess, onMapError, { enableHighAccuracy: true });
+}
+
+// Success callback for get geo coordinates
+
+var onMapSuccess = function (position) {
+
+    Latitude = position.coords.latitude;
+    Longitude = position.coords.longitude;
+
+    getMap(Latitude, Longitude);
+
+}
+
+// Get map by using coordinates
+
+function getMap(latitude, longitude) {
+
+    var mapOptions = {
+        center: new google.maps.LatLng(0, 0),
+        zoom: 1,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    map = new google.maps.Map
+    (document.getElementById("map"), mapOptions);
+
+
+    var latLong = new google.maps.LatLng(latitude, longitude);
+
+    var marker = new google.maps.Marker({
+        position: latLong
+    });
+
+    marker.setMap(map);
+    map.setZoom(15);
+    map.setCenter(marker.getPosition());
+}
+
+// Success callback for watching your changing position
+
+var onMapWatchSuccess = function (position) {
+
+    var updatedLatitude = position.coords.latitude;
+    var updatedLongitude = position.coords.longitude;
+
+    if (updatedLatitude != Latitude && updatedLongitude != Longitude) {
+
+        Latitude = updatedLatitude;
+        Longitude = updatedLongitude;
+
+        getMap(updatedLatitude, updatedLongitude);
+    }
+}
+
+// Error callback
+
+function onMapError(error) {
+    console.log('code: ' + error.code + '\n' +
+        'message: ' + error.message + '\n');
+}
+
+// Watch your changing position
+
+function watchMapPosition() {
+
+    return navigator.geolocation.watchPosition
+    (onMapWatchSuccess, onMapError, { enableHighAccuracy: true });
 }
